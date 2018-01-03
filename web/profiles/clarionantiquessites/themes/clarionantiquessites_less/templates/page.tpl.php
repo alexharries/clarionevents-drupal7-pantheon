0bb58e5ebe9d8f3c5cc8e370f7397f471927406d
Alex Harries <alex@greyhead.co.uk>
2018-01-03 10:17:03 +0000

    Zoho #311 [Olympia Art Antiques] Add H&GF logo
    Signed-off-by: Alex Harries <alex@greyhead.co.uk>

--- web/profiles/clarionantiquessites/themes/clarionantiquessites_less/templates/page.tpl.php
+++ web/profiles/clarionantiquessites/themes/clarionantiquessites_less/templates/page.tpl.php
@@ -96,95 +96,19 @@ $full_width_header_sites = [
     </div>
   <?php endif; ?>
 
-  <div id="header">
-    <div class="navbar-wrapper">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-6 col-sm-3">
-            <div class="logo">
-              <?php if ($logo): ?>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                  <img class="img-responsive" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-                </a>
-              <?php else: ?>
-                <h1 class="home-link-no-logo">
-                  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                    <?php print $site_name; ?>
-                  </a></h1>
-              <?php endif; ?>
-            </div>
-          </div>
+  <?php
+  // We show a different header for the OAA site.
+  // @todo: is it now time to start thinking about splitting this theme back
+  // into one theme per site? At least for the Olympia show sites...?
+  if (SITE_MACHINE_NAME == 'clarion-olympia-art-antiques-summer'): ?>
 
-          <div class="col-xs-6 col-sm-9">
-            <div class="row">
-              <?php
-              // On AFE, only show one full-width dates block in the header.
-              // Wide dates for the win!
-              if (SITE_MACHINE_NAME == 'clarion-antiques-for-everyone'): ?>
-                <div class="col-xs-12 col-sm-9">
-                  <div class="event-date"><?php print render($page['event_date']); ?></div>
-                </div>
-              <?php else: ?>
-                <div class="col-xs-12 col-sm-5">
-                  <div class="event-date"><?php print render($page['event_date']); ?></div>
+    <?php include 'partials/_header--clarion-olympia-art-antiques-summer.tpl.php'; ?>
 
-                </div>
-                <div class="hidden-xs col-sm-4">
-                  <?php print render($page['newsletter']); ?>
-                </div>
-              <?php endif; ?>
+  <?php else: ?>
 
-              <div class="col-xs-12 col-sm-3">
-                <?php print render($page['social_media']); ?>
-              </div>
-            </div>
+    <?php include 'partials/_header.tpl.php'; ?>
 
-          </div>
-
-          <div class="col-xs-12 col-sm-9">
-            <!--// MENU //-->
-            <div class="row">
-              <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                  <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                    </button>
-                  </div>
-
-                  <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                    <div id="navbar" class="navbar-collapse collapse">
-
-                      <?php if (!empty($primary_nav)): ?>
-                        <?php print render($primary_nav); ?>
-                      <?php endif; ?>
-                      <?php if (!empty($secondary_nav)): ?>
-                        <?php print render($secondary_nav); ?>
-                      <?php endif; ?>
-                      <?php if (!empty($page['navigation'])): ?>
-                        <?php print render($page['navigation']); ?>
-                      <?php endif; ?>
-
-                    </div>
-                  <?php endif; ?>
-                </div>
-              </nav>
-            </div>
-            <!--// END: MENU //-->
-          </div>
-
-          <!--// END: COLUMN //-->
-        </div>
-        <!--// END: ROW //-->
-      </div>
-      <!--// END: CONTAINER //-->
-    </div>
-    <!--// END: NAVBAR-WRAPPER //-->
-  </div>
-  <!--// END: HEADER //-->
+  <?php endif; ?>
 
   <?php if (!empty($page['highlighted'])): ?>
     <div class="highlighted">
